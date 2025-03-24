76ac3fefde19b8d56f799ec35cd226071b5d42b3
JorgeDanielPP <jorgedpolancop@gmail.com>
2025-03-23 23:59:55 -0400

    creando la primera clase pets

---
+++ PetHospital.Web/Models/Entities/Pets.cs
@@ -0,0 +1,17 @@
+﻿namespace PetHospital.Web.Models.Entities
+{
+    public class Pets
+    {
+        public int IdPet { get; set; }
+        public string? Raza { get; set; }
+        public int Edad { get; set; }
+        public string? Sexo { get; set; }
+        public string? Color { get; set; }
+        public string? NombrePet { get; set; }
+        public string? Direccion { get; set; }
+        public string? NombrePropietario { get; set; }
+        public string? CedulaPropietario { get; set; }
+        public string? Telefono { get; set; }
+
+    }
+}
